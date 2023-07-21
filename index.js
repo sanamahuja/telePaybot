5f1440172c52b666cd69d8d1cd49856471ba647d
Sanam <sanamahuja2000@gmail.com>
2023-07-21 13:06:23 +0530

    logs wehook request details

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const Port = 4000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.post("/", (req, res) => {
+    console.log(req.body, req.headers)
+})
+
+app.listen(Port, () => {
+    console.log("service running on port", Port)
+})
