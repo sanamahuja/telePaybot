94f9fcd070155fe526a23432638621fd20982b14
Sanam <sanamahuja2000@gmail.com>
2023-07-21 17:07:30 +0530

    sendMessage

--- index.js
+++ index.js
@@ -19,17 +19,11 @@ app.post("/", async (req, res) => {
     if (req.headers['x-telegram-bot-api-secret-token'] === SECRET_TOKEN) {
         const chat_Id = req.body.message.chat.id
         console.log(req.body)
-        try {
-            const botreply = await axios.post(`${GENERAL_URL}${BOT_TOKEN}/${BOT_METHODS.SEND_MESSAGE}`,
-                {
-                    chat_id: chat_Id,
-                    text: `hello back 👋 ${req.body.message.from.first_name}`
-                })
-            console.log(botreply)
-
-        } catch (error) {
-            console.log("send error", err)
-        }
+        await axios.post(`${GENERAL_URL}${BOT_TOKEN}/${BOT_METHODS.SEND_MESSAGE}`,
+            {
+                chat_id: chat_Id,
+                text: `hello back 👋 ${req.body.message.from?.first_name}`
+            })
     }
     res.end()
 })
