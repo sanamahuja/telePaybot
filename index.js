5734eb278c7612572d189064db87335400c366e7
sanamahuja <75626567+sanamahuja@users.noreply.github.com>
2023-08-08 16:09:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post("/", async (req, res) => {
         await axios.post(`${GENERAL_URL}${BOT_TOKEN}/${BOT_METHODS.SEND_MESSAGE}`,
             {
                 chat_id: chat_Id,
-                text:"work from home",
+                text:"",
                 reply_markup: JSON.stringify(keyBoard)
             })
     }
